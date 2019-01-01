80d13ebb0b81149147da10c74d5bd763dbd1b294
Mara Schulke <mara@schulke.xyz>
2019-01-01 02:56:12 +0100

    sec try

--- target/debug/deps/vier_gewinnt-473894558f0a9298.d
+++
@@ -1,6 +0,0 @@
-/Users/max/Desktop/personal/rust/vier-gewinnt/target/debug/deps/vier_gewinnt-473894558f0a9298: src/main.rs src/game.rs
-
-/Users/max/Desktop/personal/rust/vier-gewinnt/target/debug/deps/vier_gewinnt-473894558f0a9298.d: src/main.rs src/game.rs
-
-src/main.rs:
-src/game.rs:
--- target/debug/vier-gewinnt.d
+++
@@ -1,1 +0,0 @@
-/Users/max/Desktop/personal/rust/vier-gewinnt/target/debug/vier-gewinnt.dSYM: /Users/max/Desktop/personal/rust/vier-gewinnt/src/game.rs /Users/max/Desktop/personal/rust/vier-gewinnt/src/main.rs
