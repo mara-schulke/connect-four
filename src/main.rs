f308eceed7056b27907cd777d19e2ed00a63121a
Mara Schulke <mara@schulke.xyz>
2019-06-06 23:22:10 +0200

    feat: remove everything and start new

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,49 @@
-#![allow(dead_code)]
-#![allow(unused_imports)]
-#![allow(non_snake_case)]
-
-// use std::io;
-mod game;
-
-pub use self::game::Game;
-
 fn main() {
-    let game: Game = Game::new(
-        2,  // PLAYERCOUNT
-        10, // COLS
-        10, // ROWS
-    );
+    let game: Game {
+        players: 2,
+    }
+}
 
-    println!("{:?}", game.players );
+struct Position {
+    x: i32,
+    y: i32,
+}
+
+struct Coin {
+    player_id: u8,
+    pos: Position,
+}
+
+impl Coin {
+    pub fn new(player_id: u8, x: i32, y: i32) -> Coin {
+        Coin {
+            player_id,
+            pos: Position { x, y },
+        }
+    }
+}
+
+struct Game {
+    players: u8,
+    field: Field,
+}
+
+impl Game {
+    pub fn new(players: u8) -> Game {
+        if players < 2 { panic!("You can't play this game alone.")}
+        else if players > 4 { panic!("You can't play this game with more than 4 players.")}
+
+        Game {
+            players,
+            field: Field::new()
+        }
+    }
+}
+
+struct Field {
+    coins: Vec<Vec<Coin>>
+}
+
+impl Field {
+    
 }
