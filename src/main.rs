dae937816b98ed6d17bfb2a1862aa99f6506f2d4
Mara Schulke <mara@schulke.xyz>
2019-06-06 23:35:17 +0200

    feat: added player symbol enum

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,38 @@
 fn main() {
-    let game: Game {
-        players: 2,
+    let game: Game = Game::new(2);
+}
+
+enum PlayerSymbol {
+    First,
+    Second,
+    Third,
+    Fourth,
+}
+
+impl PlayerSymbol {
+    pub fn from_u8(s: u8) -> Option<PlayerSymbol> {
+        match s {
+            1 => Some(PlayerSymbol::First),
+            2 => Some(PlayerSymbol::Second),
+            3 => Some(PlayerSymbol::Third),
+            4 => Some(PlayerSymbol::Fourth),
+            _ => None,
+        }
+    }
+
+    pub fn as_str(&self) -> &'static str {
+        match self {
+            PlayerSymbol::First => "x",
+            PlayerSymbol::Second => "o",
+            PlayerSymbol::Third => "+",
+            PlayerSymbol::Fourth => "*",
+        }
     }
 }
 
 struct Position {
-    x: i32,
-    y: i32,
+    pub x: i32,
+    pub y: i32,
 }
 
 struct Coin {
@@ -30,20 +56,37 @@ struct Game {
 
 impl Game {
     pub fn new(players: u8) -> Game {
-        if players < 2 { panic!("You can't play this game alone.")}
-        else if players > 4 { panic!("You can't play this game with more than 4 players.")}
+        if players < 2 {
+            panic!("You can't play this game alone.")
+        } else if players > 4 {
+            panic!("You can't play this game with more than 4 players.")
+        }
 
         Game {
             players,
-            field: Field::new()
+            field: Field::new(),
         }
     }
 }
 
 struct Field {
-    coins: Vec<Vec<Coin>>
+    coins: Vec<Vec<Option<Coin>>>,
 }
 
 impl Field {
-    
+    pub fn new() -> Field {
+        // initialize a 8 x 8 field
+        Field {
+            coins: vec![
+                vec![None, None, None, None, None, None, None, None],
+                vec![None, None, None, None, None, None, None, None],
+                vec![None, None, None, None, None, None, None, None],
+                vec![None, None, None, None, None, None, None, None],
+                vec![None, None, None, None, None, None, None, None],
+                vec![None, None, None, None, None, None, None, None],
+                vec![None, None, None, None, None, None, None, None],
+                vec![None, None, None, None, None, None, None, None],
+            ],
+        }
+    }
 }
